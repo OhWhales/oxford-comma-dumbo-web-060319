9fb7c76eebce528ec39150f9f96a622669f78334
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-26 04:16:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 2 
+    return array.join(" and ")
+  elsif array.length == 3 
+    tempSentence = "#{array[0]}, #{array[1]}, and #{array[2]}"
+    return tempSentence
+  elsif array.length == 1 
+    return array.join(", ")
+  else 
+    z= array.last 
+    tempSentence = "and "
+    tempSentence = tempSentence + z 
+    array.last = tempSentence
+    return array.join(", ")
+  end 
 end
